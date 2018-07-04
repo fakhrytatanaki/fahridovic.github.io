05a811919752324df99fc3825e685f10d9814c70
Fakhry Tatanaki <fakhry8821@icloud.com>
2018-07-04 09:55:04 +0200

    Translucent Effect

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-var navbar = document.querySelector(".navbar"),collapsed = false;
+var navbar = document.querySelector(".navbar") ,
+bgnb = document.querySelector(".bgnb"), collapsed = false;
+
 function collapseNavBar()
 {
 if (collapsed === false)
-{navbar.style.height = '260px';navbar.style.backgroundColor="rgba(0,0,0,1)";collapsed = true;}
+{navbar.style.height = '260px';navbar.style.backgroundColor="rgba(0,0,0,1)";bgnb.style.opacity="0.6";collapsed = true;}
 else
 {
-{navbar.style.height = '60px';navbar.style.backgroundColor="rgba(0,0,0,0)";collapsed = false;}
+{navbar.style.height = '60px';navbar.style.backgroundColor="rgba(0,0,0,0)";bgnb.style.opacity="0";collapsed = false;}
 }
 }
